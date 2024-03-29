15ea1c473e732fc6acd09593afa5180b4e376a2f
Cairry <xuan.zu@js.design>
2024-03-29 16:27:20 +0800

    [feat]: 删除多余go func
    [feat]: 优化dashboard api
    
    [fix]: dashboard
    
    [fix]: dashboard

--- controllers/api/dashboardInfo.go
+++ controllers/api/dashboardInfo.go
@@ -60,6 +60,9 @@ func (di DashboardInfoController) GetDashboardInfo(ctx *gin.Context) {
 
 	var curAlertList []string
 	for _, v := range keys {
+		if len(curAlertList) >= 5 {
+			continue
+		}
 		alarmDistribution[di.GetCache(v).Severity] += 1
 		curAlertList = append(curAlertList, di.GetCache(v).Annotations)
 	}
