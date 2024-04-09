18109a9826d182409bee20249b945aa9e88701bb
Cairry <xuan.zu@js.design>
2024-04-09 16:48:28 +0800

    [feat]: 租户校验

--- middleware/tenant/tenant.go
+++ middleware/tenant/tenant.go
@@ -1,20 +1,46 @@
 package tenant
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 	"watchAlert/controllers/response"
+	"watchAlert/globals"
+	"watchAlert/models"
 )
+
+const TenantIDHeaderKey = "TenantID"
 
 func ParseTenantInfo() gin.HandlerFunc {
 	// 从HTTP头部获取TenantID并存储到上下文中，可以提高代码的可维护性、可重用性、安全性和性能，同时也使得错误处理和业务逻辑的实现更加高效和灵活。
 	return func(context *gin.Context) {
-		tid := context.Request.Header.Get("TenantID")
+		tid := context.Request.Header.Get(TenantIDHeaderKey)
 		if tid == "" {
 			response.Fail(context, "租户ID不能为空", "failed")
 			context.Abort()
 			return
 		}
-		// TODO 判断租户ID是否是有效的
-		context.Set("TenantID", tid)
+
+		var count int64
+		err := globals.DBCli.Model(&models.Tenant{}).Where("id = ?", tid).Count(&count).Error
+
+		if count == 0 {
+			response.Fail(context, "租户不存在", "failed")
+			context.Abort()
+			return
+		}
+
+		if err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				response.Fail(context, "租户不存在", "failed")
+			} else {
+				response.Fail(context, "数据库查询失败: "+err.Error(), "failed")
+			}
+			context.Abort()
+			return
+		}
+		
+		context.Set(TenantIDHeaderKey, tid)
+		context.Next()
 	}
 }
