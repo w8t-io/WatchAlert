d47f5a34e34987eded0168cb156fe41ad1f01d14
Cairry <xuan.zu@js.design>
2024-04-09 19:45:11 +0800

    [feat]: 优化请求日志格式

--- initialize/routes.go
+++ initialize/routes.go
@@ -4,18 +4,29 @@ import (
 	"github.com/gin-gonic/gin"
 	"watchAlert/globals"
 	"watchAlert/middleware/cors"
+	"watchAlert/middleware/requestLoggerFormatter"
 	"watchAlert/routers"
 	"watchAlert/routers/v1"
 )
 
 func InitRoute() {
-
 	globals.Logger.Sugar().Info("服务启动")
+	ginEngine := gin.New()
 
-	ginEngine := gin.Default()
+	var mode string
+	if globals.Config.Server.Mode != "" {
+		mode = globals.Config.Server.Mode
+	} else {
+		mode = "debug"
+	}
 
-	// 启用CORS中间件
-	ginEngine.Use(cors.Cors())
+	gin.SetMode(mode)
+	ginEngine.Use(
+		// 启用CORS中间件
+		cors.Cors(),
+		// 自定义请求日志格式
+		gin.LoggerWithFormatter(requestLoggerFormatter.CustomLogFormatter),
+	)
 	allRouter(ginEngine)
 
 	err := ginEngine.Run(":" + globals.Config.Server.Port)
