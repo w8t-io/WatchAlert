0e372734be28c9bff627d036ec2b2b5c16699533
Cairry <115769353+Cairry@users.noreply.github.com>
2024-08-30 17:31:43 +0800

    Optimize kubernetes event type name mapping (#79)
    * Optimize kubernetes event type name mapping

--- internal/types/kubeEvent.go
+++ internal/types/kubeEvent.go
@@ -16,62 +16,65 @@ var EventReasonLMapping = map[string][]reason{
 	"Pods": {
 		{
 			"Failed",
-			"容器启动失败",
+			"容器启动失败(Failed)",
 		},
 		{
 			"Unhealthy",
-			"容器健康状况不佳",
+			"容器健康状况不佳(Unhealthy)",
 		},
 		{
 			"CrashLoopBackOff",
-			"容器反复崩溃和重启",
+			"容器反复崩溃和重启(CrashLoopBackOff)",
 		},
 		{
 			"FailedMount",
-			"挂载卷失败",
+			"挂载卷失败(FailedMount)",
 		},
 		{
 			"FailedAttachVolume",
-			"附加卷到节点失败",
+			"附加卷到节点失败(FailedAttachVolume)",
 		},
 		{
 			"DeadlineExceeded",
-			"Pod超过其运行期限",
+			"Pod超过其运行期限(DeadlineExceeded)",
+		},
+		{
+			"FailedScheduling",
+			"Pod调度失败(FailedScheduling)",
 		},
 	},
 	"Nodes": {
 		{
 			"NodeNotReady",
-			"节点处于不可用状态",
+			"节点处于不可用状态(NodeNotReady)",
 		},
 		{
 			"NodeUnderMemoryPressure",
-			"节点处于内存压力下",
+			"节点处于内存压力下(NodeUnderMemoryPressure)",
 		},
 		{
 			"NodeUnderDiskPressure",
-			"节点处于磁盘压力下",
+			"节点处于磁盘压力下(NodeUnderDiskPressure)",
 		},
-		{},
 	},
 	"PVC/PV": {
 		{
 			"FailedBinding",
-			"PVC/PV绑定失败",
+			"PVC/PV绑定失败(FailedBinding)",
 		},
 	},
 	"HPA": {
 		{
 			"FailedRescale",
-			"调整副本数失败",
+			"调整副本数失败(FailedRescale)",
 		},
 		{
 			"FailedGetResourceMetric",
-			"获取资源指标失败",
+			"获取资源指标失败(FailedGetResourceMetric)",
 		},
 		{
 			"FailedGetExternalMetric",
-			"获取外部指标失败",
+			"获取外部指标失败(FailedGetExternalMetric)",
 		},
 	},
 }
