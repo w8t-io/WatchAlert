04a3602b986d74d333261c3d4153b7533e9e5929
thirdcountry <49510674+thirdcountry@users.noreply.github.com>
2024-09-20 14:24:32 +0800

    refactor: 💡 update deploy and auto init db (#87)
    Co-authored-by: thirdcountry <thirdcountry@gmail.com>

--- deploy/sql/rule_template_groups.sql
+++ deploy/sql/rule_template_groups.sql
@@ -1,3 +1,4 @@
+use watchalert;
 INSERT INTO `rule_template_groups` (`name`, `description`) VALUES ('Node', '服务器节点监控指标');
 INSERT INTO `rule_template_groups` (`name`, `description`) VALUES ('ApiSix', 'APISix网关监控指标');
 INSERT INTO `rule_template_groups` (`name`, `description`) VALUES ('Elasticsearch', 'Elasticsearch监控告警指标');
