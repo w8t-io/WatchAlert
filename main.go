fed42a76f75e2f02325b31e182f7a38bf3e6c3ab
Cairry <115769353+Cairry@users.noreply.github.com>
2024-11-11 10:16:35 +0800

    🚧Fix version init wrong

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"watchAlert/initialization"
+	"watchAlert/internal/global"
 )
 
+var Version string
+
 func main() {
+	global.Version = Version
 	initialization.InitBasic()
 	initialization.InitRoute()
 }
